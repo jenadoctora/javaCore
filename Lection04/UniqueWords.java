9222040386d3fb6367b4c60a3ffb9fe3b9cf9502
Ekaterina <jenadoctora@gmail.com>
2021-06-30 19:29:40 +0500

    commit_HW04

--- Lection04/UniqueWords.java
+++ Lection04/UniqueWords.java
@@ -1,2 +1,29 @@
-package javaCore.git.Lection04;public class UniqueWords {
+package javaCore.git.Lection04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueWords {
+
+    public static void main(String args[]) {
+        Set<String> set = new HashSet<>();
+        set.add("Яблоко");
+        set.add("Морковь");
+        set.add("Черешня");
+        set.add("Виноград");
+        set.add("Персик");
+        set.add("Арбуз");
+        set.add("Банан");
+        set.add("Абрикос");
+        set.add("Черешня");
+        set.add("Персик");
+        set.add("Банан");
+        set.add("Морковь");
+        set.add("Тыква");
+        set.add("Яблоко");
+
+        System.out.println(set);
+    }
+
+
 }
