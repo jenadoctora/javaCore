96169ba9775bfbc64b6cc9b07a926e46e3f97285
Ekaterina <jenadoctora@gmail.com>
2021-07-04 14:25:31 +0500

    commit_HW05

--- Lection05/AppData.java
+++ Lection05/AppData.java
@@ -1,2 +1,22 @@
-package javaCore.git.Lection05;public class AppData {
+package javaCore.git.Lection05;
+
+import java.util.Arrays;
+
+public class AppData {
+
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "AppData{" +
+                "header=" + Arrays.toString(header) +
+                ", data=" + Arrays.toString(data) +
+                '}';
+    }
 }
--- Lection05/Input.java
+++ Lection05/Input.java
@@ -1,2 +1,21 @@
-package javaCore.git.Lection05;public class Input {
+package javaCore.git.Lection05;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Input {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("writer.csv"))) {
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println("Line:" + line);
+            }
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
 }
