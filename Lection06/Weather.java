08086658aa39a69c82c328e0bb100d9ee3946808
Ekaterina <jenadoctora@gmail.com>
2021-07-07 19:49:23 +0500

    commit_HW06

--- Lection06/Weather.java
+++ Lection06/Weather.java
@@ -1,2 +1,28 @@
-package javaCore.git.Lection06;public class Weather {
+package javaCore.git.Lection06;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Weather {
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://ru.wikipedia.org")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String body = response.body().string();
+        System.out.println(response.code());
+        System.out.println(response.headers());
+        System.out.println(response.isRedirect());
+        System.out.println(response.isSuccessful());
+        System.out.println(response.protocol());
+        System.out.println(response.receivedResponseAtMillis());
+
+    }
 }
